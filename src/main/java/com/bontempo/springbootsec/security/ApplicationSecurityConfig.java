b431691d4799313f35716c0c13fec6be2a16e6f3
Paulo Bontempo <paulobontempo@gmail.com>
2020-01-28 18:23:26 -0300

    initial commit

---
+++ src/main/java/com/bontempo/springbootsec/security/ApplicationSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.bontempo.springbootsec.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails pauloBontempo = User.builder()
+                .username("paulobontempo")
+                .password(passwordEncoder.encode("paulo"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails pauloAlmeida = User.builder()
+                .username("pauloalmeida")
+                .password(passwordEncoder.encode("paulo"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+        UserDetails pauloHenrique = User.builder()
+                .username("paulohenrique")
+                .password(passwordEncoder.encode("paulo"))
+                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                pauloBontempo,
+                pauloAlmeida,
+                pauloHenrique
+        );
+    }
+}
---
+++ src/main/java/com/bontempo/springbootsec/student/Student.java
@@ -0,0 +1,10 @@
+package com.bontempo.springbootsec.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+}
